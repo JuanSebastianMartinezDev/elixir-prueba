926f17f1219b05a6c4c7bbba4c55a28f12b986ff
fabiohq <fahernandez@personalsoft.com.co>
2020-12-05 02:07:45 -0500

    primer commit

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "my_access_key" {
+  description = "Access-key-for-AWS"
+  default = "AKIARYKSG4ZK6W74JKNV"
+}
+
+variable "my_secret_key" {
+  description = "Secret-key-for-AWS"
+  default = "1NKC4Gp1T9iR4qVmaWUYFVYHJSWp25aQV422kUba"
+}
+
+variable "my_region" {
+  default = "us-east-2"
+}
+
+variable "my_ami" {
+  default = "ami-09558250a3419e7d0"
+}
+
+variable "my_instance_type"{
+  default = "t2.small"
+}
