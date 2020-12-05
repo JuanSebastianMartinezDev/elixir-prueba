5cf8dc6a04c623194e52aa27b5c276bf3d8613b5
fabiohq <fahernandez@personalsoft.com.co>
2020-12-05 02:10:33 -0500

    commit 2

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_security_group" "securitygroup" {
 	egress {
       from_port = 22
       to_port = 22
-      protocol = "tcp"
+      protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
   }
 }
